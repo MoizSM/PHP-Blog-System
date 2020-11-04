15a524a9d9e479a92dc672735a8256c401f190c5
Moiz <48330718+MoizSM@users.noreply.github.com>
2020-11-04 13:12:56 +0000

    Added user posts to profile page and added effects to profile picture

--- profile.php
+++ profile.php
@@ -1,9 +1,13 @@
 <?php
 include('./common/Header.php');
 include('./classes/Users.php');
+include('./classes/Blogs.php');
 include('./handlers/blogHandler.php');
 $user = $_SESSION['username'];
 $currentuser = new Users($conn, $user);
+
+$blogs = new AllBlogs($conn);
+$singleblogs = $blogs->displaySingleUserPosts($user);
 ?>
 
 <head>
@@ -40,8 +44,32 @@ $currentuser = new Users($conn, $user);
             <input type='submit' name='submit' class='btn' value='Create Post' />
         </form>
         <h4 style="color: green; text-align: center"><?php if (isset($msg)) {
-                                        echo $msg;
-                                    } ?></h4>
+                                                            echo $msg;
+                                                        } ?></h4>
     </div>
+
+    <div class="divider"></div>
+    <div class="userposts container">
+        <h4>All Your Posts</h4>
+        <?php foreach ($singleblogs as $item) : ?>
+            <ul class="collection with-header">
+                <li class="collection-header">
+                    <h4><?php echo $item['title'] ?></h4>
+                </li>
+                <li class="collection-item">
+                    <div><?php echo $item['body'] ?></div>
+                </li>
+                <li class="collection-item">
+                    <div>Date Created: <?php echo $item['date_created'] ?></div>
+                </li>
+            </ul>
+        <?php endforeach ?>
+    </div>
+
+    <script>
+        $(document).ready(function() {
+            $('.materialboxed').materialbox();
+        });
+    </script>
 
 </body>
