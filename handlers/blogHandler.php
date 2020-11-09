e8a681f76aa15f83f10ec5fdd04905562024a986
Moiz <48330718+MoizSM@users.noreply.github.com>
2020-11-09 11:11:35 +0000

    Revert changes

--- handlers/blogHandler.php
+++ handlers/blogHandler.php
@@ -4,14 +4,12 @@
     if(isset($_POST['submit'])){
         $title = mysqli_escape_string($conn, $_POST['title']);
         $body = mysqli_escape_string($conn, $_POST['body']);
-        $displayType = mysqli_escape_string($conn, $_POST['displayType']);
-        $displayType = (int)($displayType);
         $query = "SELECT id FROM users WHERE username='$currentuser'";
         $res = mysqli_query($conn, $query);
         if(mysqli_num_rows($res) == 1){
             $id = mysqli_fetch_array($res)['id'];
             mysqli_free_result($res);
-            $query = "INSERT INTO blogs(title, body, userId, displayType) values ('$title','$body',$id, $displayType)";
+            $query = "INSERT INTO blogs(title, body, userId) values ('$title','$body',$id)";
             mysqli_query($conn, $query);
             $msg = 'POST ADDED SUCCESFULLY';
             setcookie('msg', $msg, time() + 1, '/');
